6a2bfea314499c44c96e8f278297cad8c671d6c5
mc186 <115806113+mc186@users.noreply.github.com>
2022-10-21 13:46:38 -0400

    Update user_logic.v

--- user_logic.v
+++ user_logic.v
@@ -1,11 +1,7 @@
-module adder (input wire clk,
-              input reg [31:0] a,
+module adder (input reg [31:0] a,
               input reg [31:0] b,
               output reg [31:0] c);
 
-  always@(posedge clk)
-	begin
-	    c <= a + b;
-	end
+  assign c= a + b;
 endmodule
 
